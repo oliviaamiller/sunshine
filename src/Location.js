7dfd4e943cfa52986701121d766530a8acba8985
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-28 16:00:28 -0700

    fixed some errors

--- src/Location.js
+++ src/Location.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './App.css';
 
 export default function Location({ location }) {
   return (
-    <div>{location.long}</div>
+    <div className='location'>{location.long}</div>
   );
 }
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Location from './Location';
+import './App.css';
 
 export default function LocationList({ locations }) {
   return (
-    <div>
+    <div className='locations'>
       Locations
       {
         locations.map((location, i) => <Location key={`${location}-${i}`} location={location}/>)
