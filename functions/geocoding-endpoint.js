5c3931cd847067c699634030ac94ae793c3b41eb
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-28 12:54:24 -0800

    created geocoding endpoint and able to retrieve data

---
+++ functions/geocoding-endpoint.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+exports.handler = async (event, context) => {
+  try {
+    const geocodeResponse = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${event.queryStringParameters.zip}&appid=${process.env.WEATHER_API_KEY}`);
+    const geoJson = await geocodeResponse.json();
+    return {
+      statusCode: 200,
+    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
+      body: JSON.stringify(geoJson),
+    };
+  } catch (error) {
+  // eslint-disable-next-line
+  console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -3,19 +3,21 @@ import LocationList from './LocationList';
 
 
 export default function SearchPage() {
-  const [userLocation, setUserLocation] = useState('Salem, Oregon');
+  const [userZip, setUserZip] = useState('97215');
   const [userCoords, setUserCoords] = useState({});
   const [results, setResults] = useState([]);
   const [locations, setLocations] = useState([]);
 
 
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     // take user location convert it to coords
     console.log('It works!!');
     // using our geocoding api and store it in state
-
+    const response = await fetch(`/.netlify/functions/geocoding-endpoint?zip=${userZip}`);
+    const json = await response.json();
+    console.log(json);
     // using the user coords in state go find 8 location coords within 55miles
     
     //store locations in an array
@@ -26,7 +28,7 @@ export default function SearchPage() {
     <>
       <div>
         <form onSubmit={handleSubmit}>
-          <input placeholder='Current Location' value={userLocation} onChange={e => setUserLocation(e.target.value)}/>
+          <input placeholder='Current Location' value={userZip} onChange={e => setUserZip(e.target.value)}/>
           <button type='submit'>Search</button>
         </form>
       </div>
