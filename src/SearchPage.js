456210754c62526408c035b773f2e811701cfba8
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-28 12:17:28 -0800

    added more state, set up form input, added pseudo code

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LocationList from './LocationList';
 
+
 export default function SearchPage() {
+  const [userLocation, setUserLocation] = useState('Salem, Oregon');
+  const [userCoords, setUserCoords] = useState({});
+  const [results, setResults] = useState([]);
+  const [locations, setLocations] = useState([]);
 
-  function handleSubmit() {
 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // take user location convert it to coords
+    console.log('It works!!');
+    // using our geocoding api and store it in state
+
+    // using the user coords in state go find 8 location coords within 55miles
+    
+    //store locations in an array
+
+    //pass location array as a prop to LocationList
   }
   return (
     <>
       <div>
-        <form>
-          <input placeholder='Current Location'/>
-          <button>Search</button>
+        <form onSubmit={handleSubmit}>
+          <input placeholder='Current Location' value={userLocation} onChange={e => setUserLocation(e.target.value)}/>
+          <button type='submit'>Search</button>
         </form>
       </div>
       <div>
