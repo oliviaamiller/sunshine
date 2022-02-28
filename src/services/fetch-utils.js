47b37684a57db8dcb13e67c53d3c70141dd7a84b
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-28 10:40:58 -0800

    set up files, added .env keys, started fetch-utils

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,23 @@
+import { client, checkError } from './client';
+
+export async function getUser() {
+  return client.auth.session;
+}
+
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
+
+export async function logOut() {
+  await client.auth.signOut();
+
+  return window.location.href = '../';
+}
