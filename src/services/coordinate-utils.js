b8eb960abecda31deb5c36ac6763444c75e53238
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-01 11:53:07 -0800

    created buildCoordsByDistance func that loops through an array of distances

--- src/services/coordinate-utils.js
+++ src/services/coordinate-utils.js
@@ -1,8 +1,8 @@
-export function getCoordinates(lat, long, dx, dy) {
+function getCoordinates(lat, long, dx, dy) {
   const EARTH_RADIUS = 6371000.0;
 
-  const newLat = lat + (dx / EARTH_RADIUS) * (180 / Math.PI);
-  const newLong = long + (dy / EARTH_RADIUS) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);
+  const newLat = Number((lat + (dx / EARTH_RADIUS) * (180 / Math.PI)).toFixed(8));
+  const newLong = Number((long + (dy / EARTH_RADIUS) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180)).toFixed(8));
 
   
   return {
@@ -12,5 +12,19 @@ export function getCoordinates(lat, long, dx, dy) {
 
 }
 
+//Dist = distance in meters
+export function buildCoordsByDistance(lat, long, dist) {
+  const coordObj = [];
+  let flag = dist;
+  const step = 40233.6;
 
+  while (flag > -dist - 1000) {
+    coordObj.push(getCoordinates(lat, long, flag, 0));
+    coordObj.push(getCoordinates(lat, long, 0, flag));
+    
+    flag = flag - step;
+  }
+
+  return coordObj;
+}
 
