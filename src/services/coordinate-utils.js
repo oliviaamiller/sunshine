9fd24ea625d4828823db0678f37ac5b9b4837a9b
Victor Caruso <vic.caruso@gmail.com>
2022-03-01 10:05:50 -0800

    formula for new coordinates now working

--- src/services/coordinate-utils.js
+++ src/services/coordinate-utils.js
@@ -1,5 +1,16 @@
-// export function getCoordinates(lat, long, dist, bearing) {
-// const EARTH_RADIUS = 6378.1;
-// const newLat = Math.asin( Math.sin(lat) * Math )
-// };
+export function getCoordinates(lat, long) {
+  const EARTH_RADIUS = 6371000.0;
+
+  const newLat = lat + (80467.2 / EARTH_RADIUS) * (180 / Math.PI);
+  const newLong = long + (0 / EARTH_RADIUS) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);
+
+  
+  return {
+    lat: newLat,
+    long: newLong
+  };
+
+}
+
+
 
