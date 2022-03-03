677a3a44dff92801f001a4a447154daca452c8c5
Victor Caruso <vic.caruso@gmail.com>
2022-03-02 16:19:30 -0800

    calculating the distance from the user for each location rendered

--- src/Location.js
+++ src/Location.js
@@ -2,33 +2,31 @@ import React from 'react';
 import './App.css';
 import { addToFavorites, getFavorites } from './services/fetch-utils';
 import { Link } from 'react-router-dom';
+import { calculateDistanceAsCrowFlies } from './services/coordinate-utils';
 import SearchDetailPage from './SearchDetailPage';
 
 
 
-export default function Location({ location }) {
-  // const favorite = isFavorite(location.id);
-  // console.log(favorite);
+export default function Location({ location, userCoords }) {
+  const distanceFromUser = calculateDistanceAsCrowFlies(userCoords.lat, userCoords.long, location.coord.lat, location.coord.lon);
+
   async function handleClick() {
-    
     const favoriteObj = {
       city_name: location.name,
       lat: location.coord.lat,
       long: location.coord.lon,
-      // city_id: location.id
     };
     await addToFavorites(favoriteObj);
     await getFavorites(); 
     
   }
   
-
   function temp() {
     let f = 1.8 * (
       `${location.main.temp}` - 273) + 32;
     return Math.round(f);
   }
-  // <SearchDetailPage location={location}/>;
+
   return (
     <>
       <Link to={`/searchdetail/${location.id}`}>
@@ -36,16 +34,10 @@ export default function Location({ location }) {
           <p>{location.name}</p>
           <p>{location.weather[0].main && 'Sunny'}</p>
           <p>{temp()} °F</p>
+          <p>{`${Math.round(distanceFromUser)} km from you`}</p>
         </div>
       </Link>
       
     </>
-    
-    
-          
-        
-
-      
-    
   );
 }
--- src/LocationList.js
+++ src/LocationList.js
@@ -4,19 +4,20 @@ import FavoriteItem from './FavoriteItem';
 import { useLocation } from 'react-router-dom';
 import SearchDetailPage from './SearchDetailPage';
 import './App.css';
+import { calculateDistanceAsCrowFlies } from './services/coordinate-utils';
 
-export default function LocationList({ locations, fetchAndRefresh }) {
+export default function LocationList({ locations, fetchAndRefresh, userCoords }) {
   const path = useLocation(); 
 
   if (path.pathname.includes('search')) {
     
     const clearLocations = locations.filter(location => location.weather[0].main === 'Clear');
-    console.log(locations);
+
     return (
       <div className='locations'>
         {
           clearLocations.length
-            ? clearLocations.map((location, i) => <Location key={`${location}-${i}`} location={location} />)
+            ? clearLocations.map((location, i) => <Location key={`${location}-${i}`} location={location} userCoords={userCoords}/>)
             : <p>its not sunny</p>
         }
       </div>
@@ -24,16 +25,14 @@ export default function LocationList({ locations, fetchAndRefresh }) {
   }
 
   if (path.pathname.includes('favorites')) {
-  
     return (
       locations.map((location, i) => <FavoriteItem key={`${location}-${i}`} location={location} fetchAndRefresh={fetchAndRefresh} />)
     );
   }
+
   if (path.pathname.includes('searchdetail')) {
     return (
       locations.map((location, i) => <SearchDetailPage key={`${location}-${i}`} location={location} fetchAndRefresh={fetchAndRefresh} />)
     );
   }
-
-
 }
