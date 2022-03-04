f6f01eacb1ea8c3d76c668b60931f2b225960dfe
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-04 09:21:18 -0800

    added about data into aboutdata

--- src/FavoriteDetail.js
+++ src/FavoriteDetail.js
@@ -3,12 +3,13 @@ import { useParams } from 'react-router-dom';
 import { getSingleLocation } from './services/fetch-utils';
 import { getFutureWeather } from './services/api-utils';
 import WeatherDetails from './WeatherDetails';
+import Spinner from './Spinner';
 
 export default function FavoriteDetail() {
   const [locationDetails, setLocationDetails] = useState('');
   const [futureForecast, setFutureForecast] = useState([]);
   const params = useParams();
-
+  const [isLoading, setIsLoading] = useState(false);
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat'];
@@ -27,55 +28,37 @@ export default function FavoriteDetail() {
   }, [params.id, locationDetails.lat]);
 
   useEffect(() => {
+
     async function fetchFuture() {
+      setIsLoading(true);
       const fiveDay = await getFutureWeather(locationDetails.lat, locationDetails.long);
       
       setFutureForecast(fiveDay);
+      setIsLoading(false);
     }
     if (!futureForecast.current && locationDetails.lat){
       fetchFuture();
     }
   }, [futureForecast, locationDetails.lat, locationDetails.long]);
 
-
-  // function currentTemp() {
-  //   let f = 1.8 * (
-  //     `${locationDetails.temp}` - 273) + 32;
-  //   return Math.round(f);
-  // }
-  // function feelsLike() {
-  //   let f = 1.8 * (
-  //     `${locationDetails.feels_like}` - 273) + 32;
-  //   return Math.round(f);
-  // }
-  // function tempmin() {
-  //   let f = 1.8 * (
-  //     `${locationDetails.temp_min}` - 273) + 32;
-  //   return Math.round(f);
-  // }
-  // function tempmax() {
-  //   let f = 1.8 * (
-  //     `${locationDetails.temp_max}` - 273) + 32;
-  //   return Math.round(f);
-  // }
-
-  // function date(dt) {
-  //   return new Date(dt * 1000).toUTCString();
-  // }
-
-  
-
-
   if (futureForecast.lat) {
     return (
-
+      
       <div className='location'>
-        <p>{locationDetails.city_name}</p>
-        {
-          futureForecast.daily.map((day, i) => <WeatherDetails key={`${day.dt} + ${i}`} dayForecast={day} months={months} days={days}/>)
+        {isLoading
+          ? <Spinner />
+          : <>
+            <p>{locationDetails.city_name}</p>
+          
+            {futureForecast.daily.map((day, i) => <WeatherDetails key={`${day.dt} + ${i}`} dayForecast={day} months={months} days={days}/>)}
+          </>
+              
+        
+          
         }
-
       </div>
+     
+      
     );
   } else { return null; }
 }
--- src/FavoritesPage.js
+++ src/FavoritesPage.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import LocationList from './LocationList';
 import { getFavorites, getUser } from './services/fetch-utils';
+import Spinner from './Spinner';
 
 export default function FavoritesPage() {
   const [locations, setLocations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchAndRefresh() {
+    setIsLoading(true);
     const user = await getUser();
     const favoriteList = await getFavorites(user);
     setLocations(favoriteList);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -17,7 +21,10 @@ export default function FavoritesPage() {
 
   return (
     <div className='favorites-page'>
-      <LocationList locations={locations} fetchAndRefresh={fetchAndRefresh} />
+      {isLoading 
+        ? <Spinner />
+        : <LocationList locations={locations} fetchAndRefresh={fetchAndRefresh} />
+      }
     </div>
   );
 }
